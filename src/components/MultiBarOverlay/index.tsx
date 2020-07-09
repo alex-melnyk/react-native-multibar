62adbc24863ab4f60cf616ab3bc077dd4280d961
brendan1809 <brendan543210@gmail.com>
2020-07-09 15:10:37 +0800

    feat: when press other tab will close the overlay

--- src/components/MultiBarOverlay/index.tsx
+++ src/components/MultiBarOverlay/index.tsx
@@ -1,41 +1,46 @@
-import * as React from 'react';
-import { Animated } from 'react-native';
+import * as React from "react";
+import { Animated } from "react-native";
 
-import { MultiBarContext, } from '../../context';
-import { MultiBarOverlayProps, MultiBarPassThroughParams } from '../../types';
-import { styles } from './Styles';
+import { MultiBarContext } from "../../context";
+import { MultiBarOverlayProps, MultiBarPassThroughParams } from "../../types";
+import { styles } from "./Styles";
 
 const COMMON_DEGREES = 180;
 
-type Props = Pick<MultiBarPassThroughParams, 'params'>;
+type Props = Pick<MultiBarPassThroughParams, "params">;
 
-export const MultiBarOverlay: React.FC<Props> = ({
-  params
-}) => {
+export const MultiBarOverlay: React.FC<Props> = ({ params }) => {
   const {
     data,
     extrasVisible,
     overlayProps,
-    setExtrasVisible
+    setExtrasVisible,
   } = React.useContext(MultiBarContext);
 
-  const {
-    expandingMode,
-    iconSize,
-    overlayRadius
-  } = React.useMemo<Required<MultiBarOverlayProps>>(() => {
-    return Object.assign<Required<MultiBarOverlayProps>, MultiBarOverlayProps>({
-      expandingMode: 'staging',
-      iconSize: 30,
-      overlayRadius: 80
-    }, overlayProps || {});
+  const { expandingMode, iconSize, overlayRadius } = React.useMemo<
+    Required<MultiBarOverlayProps>
+  >(() => {
+    return Object.assign<Required<MultiBarOverlayProps>, MultiBarOverlayProps>(
+      {
+        expandingMode: "staging",
+        iconSize: 30,
+        overlayRadius: 80,
+      },
+      overlayProps || {}
+    );
   }, [overlayProps]);
 
   const iconSizeHalf = React.useMemo(() => iconSize / 2, [iconSize]);
-  const surfaceSize = React.useMemo(() => (overlayRadius * 2) + iconSize, [iconSize, overlayRadius]);
+  const surfaceSize = React.useMemo(() => overlayRadius * 2 + iconSize, [
+    iconSize,
+    overlayRadius,
+  ]);
   const surfaceSizeHalf = React.useMemo(() => surfaceSize / 2, [surfaceSize]);
   const angleStep = React.useMemo(() => COMMON_DEGREES / data.length, [data]);
-  const animations = React.useMemo(() => data.map(() => new Animated.Value(extrasVisible ? 1 : 0)), [data]);
+  const animations = React.useMemo(
+    () => data.map(() => new Animated.Value(extrasVisible ? 1 : 0)),
+    [data]
+  );
   const overlayHeight = React.useMemo(() => {
     return surfaceSizeHalf * (surfaceSize / overlayRadius / 2);
   }, [surfaceSizeHalf, surfaceSize, overlayRadius]);
@@ -43,73 +48,100 @@ export const MultiBarOverlay: React.FC<Props> = ({
   React.useEffect(() => {
     const animate = Animated.spring;
 
-    const animationsList = animations.map((anim, idx) => animate(anim, {
-      toValue: extrasVisible ? 1 : 0,
-      useNativeDriver: false
-    }));
+    const animationsList = animations.map((anim, idx) =>
+      animate(anim, {
+        toValue: extrasVisible ? 1 : 0,
+        useNativeDriver: false,
+      })
+    );
 
-    const animator = expandingMode === 'staging'
-      ? Animated.stagger(100, animationsList)
-      : Animated.parallel(animationsList);
+    const animator =
+      expandingMode === "staging"
+        ? Animated.stagger(100, animationsList)
+        : Animated.parallel(animationsList);
 
     animator.start();
 
     return () => animator.stop();
-  }, [extrasVisible]);
+  }, [extrasVisible, data]);
 
-  const itemsList = React.useMemo(() => data.map((extrasRender, idx) => {
-    const handleTouchEnd = () => {
-      setExtrasVisible(false);
-    };
+  React.useEffect(() => {
+    setExtrasVisible(false);
+  }, [data]);
 
-    const angle = COMMON_DEGREES + (angleStep * idx) + (angleStep / 2);
+  const itemsList = React.useMemo(
+    () =>
+      data.map((extrasRender, idx) => {
+        const handleTouchEnd = () => {
+          setExtrasVisible(false);
+        };
 
-    const x = overlayRadius * Math.cos(angle * Math.PI / COMMON_DEGREES) + (surfaceSizeHalf - iconSizeHalf);
-    const y = overlayRadius * Math.sin(angle * Math.PI / COMMON_DEGREES) + (surfaceSizeHalf);
+        const angle = COMMON_DEGREES + angleStep * idx + angleStep / 2;
 
-    const left = animations[idx].interpolate({
-      inputRange: [0, 1],
-      outputRange: [surfaceSizeHalf - iconSizeHalf, x]
-    });
+        const x =
+          overlayRadius * Math.cos((angle * Math.PI) / COMMON_DEGREES) +
+          (surfaceSizeHalf - iconSizeHalf);
+        const y =
+          overlayRadius * Math.sin((angle * Math.PI) / COMMON_DEGREES) +
+          surfaceSizeHalf;
 
-    const top = animations[idx].interpolate({
-      inputRange: [0, 1],
-      outputRange: [surfaceSize, y]
-    });
+        const left = animations[idx].interpolate({
+          inputRange: [0, 1],
+          outputRange: [surfaceSizeHalf - iconSizeHalf, x],
+        });
 
-    const rotateZ = animations[idx].interpolate({
-      inputRange: [0, 1],
-      outputRange: [90, 0]
-    });
+        const top = animations[idx].interpolate({
+          inputRange: [0, 1],
+          outputRange: [surfaceSize, y],
+        });
 
-    return (
-      <Animated.View
-        key={`extra_item_${idx}`}
-        style={[styles.itemContainer, {
-          left,
-          top,
-          width: iconSize,
-          height: iconSize,
-          transform: [
-            { rotateZ }
-          ]
-        }]}
-        onTouchEnd={handleTouchEnd}
-      >
-        {extrasRender({
-          params: params
-        })}
-      </Animated.View>
-    )
-  }), [animations, angleStep, data, iconSize, overlayRadius, surfaceSizeHalf, iconSizeHalf]);
+        const rotateZ = animations[idx].interpolate({
+          inputRange: [0, 1],
+          outputRange: [90, 0],
+        });
+
+        return (
+          <Animated.View
+            key={`extra_item_${idx}`}
+            style={[
+              styles.itemContainer,
+              {
+                left,
+                top,
+                width: iconSize,
+                height: iconSize,
+                transform: [{ rotateZ }],
+              },
+            ]}
+            onTouchEnd={handleTouchEnd}
+          >
+            {extrasRender({
+              params: params,
+            })}
+          </Animated.View>
+        );
+      }),
+    [
+      animations,
+      angleStep,
+      data,
+      iconSize,
+      overlayRadius,
+      surfaceSizeHalf,
+      iconSizeHalf,
+    ]
+  );
 
   return (
     <Animated.View
       pointerEvents="box-none"
-      style={[styles.container, {
-        width: surfaceSize,
-        height: overlayHeight
-      }]}
+      style={[
+        styles.container,
+        {
+          width: surfaceSize,
+          height: overlayHeight,
+        },
+      ]}
     >
       {itemsList}
     </Animated.View>
